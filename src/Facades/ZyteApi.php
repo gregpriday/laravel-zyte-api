81442a1e1f35c6a363d0aa93d53df2f36e1efbe3
Greg Priday <greg@siteorigin.com>
2024-03-10 14:10:54 +0200

    Added methods to Facade

--- src/Facades/ZyteApi.php
+++ src/Facades/ZyteApi.php
@@ -5,6 +5,12 @@ namespace GregPriday\ZyteApi\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static array extract(array $urls, ?callable $processCallback = null, array $additionalArgs = [])
+ * @method static string|array extractHttpBody(string|array $url)
+ * @method static string|array extractBrowserHtml(string|array $url)
+ * @method static \stdClass|array extractArticle(string|array $url)
+ * @method static string htmlToCleanMarkdown(string $html)
+ *
  * @see \GregPriday\ZyteApi\ZyteApi
  */
 class ZyteApi extends Facade
